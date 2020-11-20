77e399f93a3fc6425e4e6929479f8b77290d9b78
Srinivasa Vasu <srinivasan.surprise@gmail.com>
2020-11-20 10:15:32 +0800

    lifecyle 0.9.3 update

--- stacks/scripts/create-stack.sh
+++ stacks/scripts/create-stack.sh
@@ -1,13 +1,20 @@
 #!/usr/bin/env bash
 
-usage() { echo "Usage: $0 [-v <image version>]" 1>&2; exit 1; }
+usage() { echo "Usage: $0 [-v <image version> -b <ubuntu base os version>]" 1>&2; exit 1; }
 version=tiny
+base_os=bionic
 
-while getopts "v:" o; do
+set -x
+
+while getopts "v:b:" o; do
   case "${o}" in
     v)
       version=${OPTARG}
-      [[ -n ${version} ]] || usage
+      [[ -n ${version} ]] || version=tiny
+      ;;
+    b)
+      base_os=${OPTARG}
+      [[ -n ${base_os} ]] || base_os=bionic
       ;;
     *)
       usage
@@ -15,22 +22,22 @@ while getopts "v:" o; do
   esac
 done
 
-docker pull ubunutu:bionic
+docker pull ubunutu:${base_os}
 
 scripts_dir=$(cd $(dirname $0) && pwd)
 dir=${scripts_dir}/../tiny
 
-base_image=humourmind/cnb-base:${version}
-run_image=humourmind/cnb-run:${version}
-#run_base_image=cloudfoundry/run:tiny
-run_base_image=gcr.io/distroless/base
-build_image=humourmind/cnb-build:${version}
+base_image=humourmind/cnb-${base_os}-base:${version}
+run_image=humourmind/cnb-${base_os}-run:${version}
+run_base_image=gcr.io/paketo-buildpacks/run:tiny-cnb
+#run_base_image=gcr.io/distroless/base
+build_image=humourmind/cnb-${base_os}-build:${version}
 stack_id="io.buildpacks.stacks.bionic"
 cnb_uid=1000
 cnb_gid=1000
-docker build -t "${base_image}" "$dir/base"
-docker build --build-arg "base_image=${base_image}" --build-arg "stack_id=${stack_id}" --build-arg "cnb_uid=${cnb_uid}" --build-arg "cnb_gid=${cnb_gid}" -t "${build_image}"  "$dir/build"
-docker build --build-arg "base_image=${run_base_image}" --build-arg "stack_id=${stack_id}" --build-arg "cnb_uid=${cnb_uid}" --build-arg "cnb_gid=${cnb_gid}" -t "${run_image}" "$dir/run"
+docker build -t "${base_image}" "$dir/base" --build-arg "base_os=${base_os}"
+docker build --build-arg "base_os=${base_os}" --build-arg "base_image=${base_image}" --build-arg "stack_id=${stack_id}" --build-arg "cnb_uid=${cnb_uid}" --build-arg "cnb_gid=${cnb_gid}" -t "${build_image}"  "$dir/build"
+docker build --build-arg "base_os=${base_os}" --build-arg "base_image=${run_base_image}" --build-arg "stack_id=${stack_id}" --build-arg "cnb_uid=${cnb_uid}" --build-arg "cnb_gid=${cnb_gid}" -t "${run_image}" "$dir/run"
 
 echo "To publish these images:"
 for image in "${base_image}" "${run_image}" "${build_image}"; do
